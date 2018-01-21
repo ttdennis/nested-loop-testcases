78843952531e8f282786b414993390e4f3e5e36d
shocky698 <32130890+shocky698@users.noreply.github.com>
2018-01-21 01:05:30 +0100

    Add files via upload

---
+++ tline.js
@@ -0,0 +1,1178 @@
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
+function mul(a,b){
+	var ans	= 0;
+	var i = 0;
+	var j = 0;
+	for (i=0; i<a; i++){
+		for (j=0; j<b; j++){
+			ans += a*b
+	}
+	}
+return ans;
+}
